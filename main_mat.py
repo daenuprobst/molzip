7199d66b6a6aecd45e88e2100825d5931099bf8d
RishikeshMagar <rishikeshmagar007@gmail.com>
2023-07-21 22:20:19 -0700

    MOF_toy

--- main_mat.py
+++ main_mat.py
@@ -298,8 +298,6 @@ def benchmark(configs: List[Dict[str, Any]]) -> None:
                 test_preds = regress(X_train, y_train, X_test, config["k"])
 
                 # Compute metrics
-                print("y_valid", y_valid)
-                print("valid_preds", valid_preds)
                 valid_rmse = mean_squared_error(y_valid, valid_preds, squared=False)
                 valid_mae = mean_absolute_error(
                     y_valid,
