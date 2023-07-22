4d54987d3be61595da74389b5310f6a8bd459095
RishikeshMagar <rishikeshmagar007@gmail.com>
2023-07-21 21:22:50 -0700

    MOF_toy

--- main_mat.py
+++ main_mat.py
@@ -331,17 +331,17 @@ def benchmark(configs: List[Dict[str, Any]]) -> None:
 def main():
     benchmark(
         [
-            # {
-            #     "dataset": "freesolv",
-            #     "splitter": "random",
-            #     "task": "regression",
-            #     "k": 25,
-            #     "augment": 0,
-            #     "preprocess": False,
-            #     "sub_sample": 0.0,
-            #     "is_imbalanced": False,
-            #     "n": 4,
-            # },
+            {
+                "dataset": "freesolv",
+                "splitter": "random",
+                "task": "regression",
+                "k": 25,
+                "augment": 0,
+                "preprocess": False,
+                "sub_sample": 0.0,
+                "is_imbalanced": False,
+                "n": 4,
+            },
             # {
             #     "dataset": "delaney",
             #     "splitter": "random",
@@ -497,17 +497,17 @@ def main():
             #     "n": 1,
             # },
 
-            {
-                "dataset": "MOF",
-                "splitter": "random",
-                "task": "regression",
-                "k": 25,
-                "augment": 0,
-                "preprocess": True,
-                "sub_sample": 0.0,
-                "is_imbalanced": False,
-                "n": 4,
-            },
+            # {
+            #     "dataset": "MOF",
+            #     "splitter": "random",
+            #     "task": "regression",
+            #     "k": 25,
+            #     "augment": 0,
+            #     "preprocess": True,
+            #     "sub_sample": 0.0,
+            #     "is_imbalanced": False,
+            #     "n": 4,
+            # },
 
         ]
     )
